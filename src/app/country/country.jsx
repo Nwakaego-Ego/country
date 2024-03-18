b35b0e7e2ff65eb98d112816fa964cc8cc33d983
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-03-19 00:06:45 +0100

    working on fetching data

---
+++ src/app/country/country.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function Country() {
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = "553|UgsZByghCuETcdTVJYdWl3epZvCEqdYsjM4CQg1C";
+        const apiUrl = "https://restfulcountries.com/api/v1/countries";
+        const headers = {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await fetch(apiUrl, { headers });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+
+        const data = await response.json();
+        setCountry(data); // Assuming data is an array of countries
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Dependency array
+
+  return (
+    <div>
+      {country.length > 0 ? (
+        country.map((countryInfo, index) => (
+          <div key={index}>
+            <p>{countryInfo.name}</p>
+            {/* Render other country information here */}
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
